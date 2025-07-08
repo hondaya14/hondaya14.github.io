6b5034d6f1f606580ae142b210f6af120e2d3013
Yasuhisa Honda <nqvno14@gmail.com>
2025-07-08 23:06:28 +0900

    Simplify blog design

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -6,8 +6,8 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="min-h-screen bg-[#f7f7f7] text-[#222] font-sans">
       {children}
-    </>
+    </div>
   );
 }
