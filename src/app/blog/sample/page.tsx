7fe24fb8e71a43f8c2e6a52a4a9cef9dad3ad3a7
Yasuhisa Honda <nqvno14@gmail.com>
2025-11-22 00:25:55 +0900

    submodule

--- src/app/blog/sample/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import MDXContent from './content.mdx'
-import { mdxCodeComponents } from '@/components/MdxCode'
-
-export default function Page() {
-  return <MDXContent components={mdxCodeComponents} />
-}
